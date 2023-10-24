93e51158365ef750f8e92c5c8b34e9316eeb72e8
seta-trungvuong <tvuong@veritone.com>
2023-10-24 13:53:22 +0700

    check coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };

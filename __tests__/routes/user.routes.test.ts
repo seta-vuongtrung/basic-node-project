38a809d8ed5d44e06b56ce6a16a7d56e7aba8c96
seta-trungvuong <tvuong@veritone.com>
2023-10-24 14:11:55 +0700

    update test

--- __tests__/routes/user.routes.test.ts
+++ __tests__/routes/user.routes.test.ts
@@ -42,4 +42,18 @@ describe("User routes", () => {
       expect(res.body.message).toEqual("not found user with id 4!");
     });
   });
+
+  describe("Delete user by Id", () => {
+    test("found user by Id should return status code 200.", async () => {
+      const res = await request(app).delete("/users/1");
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.message).toEqual("Delete user with id 1 successful!");
+    });
+
+    test("not found user by Id should return status code 404 & error message", async () => {
+      const res = await request(app).delete("/users/4");
+      expect(res.statusCode).toEqual(404);
+      expect(res.body.message).toEqual("Not found user with id 4!");
+    });
+  });
 });
